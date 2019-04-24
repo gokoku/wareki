17288842eccb6597f778778a32fc011883949299
george <george.6809@gmail.com>
2019-04-24 18:11:27 +0900

    Updates

---
+++ precache-manifest.7493927fc57622af5308fcc648783347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e144dc87184e32d62b7652c365e8d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c90fca6847c1db26c749",
+    "url": "/static/css/main.e4b13b02.chunk.css"
+  },
+  {
+    "revision": "0b48b3d075db2ef058ca",
+    "url": "/static/js/2.16299f92.chunk.js"
+  },
+  {
+    "revision": "c90fca6847c1db26c749",
+    "url": "/static/js/main.72ff8077.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
